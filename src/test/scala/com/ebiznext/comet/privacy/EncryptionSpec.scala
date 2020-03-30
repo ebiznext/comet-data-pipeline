ab96b6eed1678273eb7c28879b5e7bd704a7dca5
Hayssam Saleh <Hayssam.saleh@ebiznext.com>
2020-03-30 12:14:51 +0200

    Renamed Encryption to PrivacyEngine since it covers Encryption & Masking

--- src/test/scala/com/ebiznext/comet/privacy/EncryptionSpec.scala
+++ src/test/scala/com/ebiznext/comet/privacy/EncryptionSpec.scala
@@ -1,19 +1,19 @@
 package com.ebiznext.comet.job.convert
 
 import com.ebiznext.comet.TestHelper
-import com.ebiznext.comet.privacy.{Email, Encryption, IPv4, IPv6, Initials, Mask}
+import com.ebiznext.comet.privacy.{Email, PrivacyEngine, IPv4, IPv6, Initials, Mask}
 
 class EncryptionSpec extends TestHelper {
 
   new WithSettings() {
     "Parsing a single arg encryption algo" should "succeed" in {
-      val (algo, params)  = Encryption.parse("com.ebiznext.comet.privacy.Approx(10)")
+      val (algo, params)  = PrivacyEngine.parse("com.ebiznext.comet.privacy.Approx(10)")
       algo should equal("com.ebiznext.comet.privacy.Approx")
       params.head shouldBe a [Int]
       params should equal (List(10))
     }
     "Parsing a multiple arg encryption algo" should "succeed" in {
-      val (algo, params)  = Encryption.parse("package.SomeAlgo('X', \"Hello\", 12, false, 12.34)")
+      val (algo, params)  = PrivacyEngine.parse("package.SomeAlgo('X', \"Hello\", 12, false, 12.34)")
       algo should equal("package.SomeAlgo")
       params should have length 5
       params(0) shouldBe a [Char]
@@ -25,23 +25,23 @@ class EncryptionSpec extends TestHelper {
     }
 
     "Initials Masking Firstname" should "succeed" in {
-      val result = Initials.encrypt("John")
+      val result = Initials.crypt("John")
       result shouldBe "J."
     }
 
     "Initials Masking Composite name" should "succeed" in {
-      val result = Initials.encrypt("John Doe")
+      val result = Initials.crypt("John Doe")
       result shouldBe "J.D."
     }
 
     "Email Masking" should "succeed" in {
-      val result = Email.encrypt("john@doe.com")
+      val result = Email.crypt("john@doe.com")
       result should have length "527bd5b5d689e2c32ae974c6229ff785@doe.com".length
       result should endWith ("@doe.com")
     }
 
     "IPv4 Masking" should "succeed" in {
-      val result = IPv4.encrypt("192.168.2.1")
+      val result = IPv4.crypt("192.168.2.1")
       result shouldBe "192.168.2.0"
     }
 
@@ -51,7 +51,7 @@ class EncryptionSpec extends TestHelper {
     }
 
     "IPv6 Masking" should "succeed" in {
-      val result = IPv6.encrypt("2001:db8:0:85a3::ac1f:8001")
+      val result = IPv6.crypt("2001:db8:0:85a3::ac1f:8001")
       result shouldBe "2001:db8:0:85a3::ac1f:0"
     }
 
@@ -61,7 +61,7 @@ class EncryptionSpec extends TestHelper {
     }
 
     "Generic Masking" should "succeed" in {
-      val result = Mask.encrypt("+3360102030405", '*', 8, 4, 2)
+      val result = Mask.crypt("+3360102030405", '*', 8, 4, 2)
       result shouldBe "+336********05"
     }
   }
