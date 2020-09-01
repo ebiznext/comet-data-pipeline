25edbdd3e43ac23f397547b842c160cf3d42f0ad
Mohamad Kassir <mohamad.kassir@ebiznext.com>
2020-09-01 17:54:48 +0200

    minor correction en error msg

--- src/main/scala/com/ebiznext/comet/job/ingest/DsvIngestionJob.scala
+++ src/main/scala/com/ebiznext/comet/job/ingest/DsvIngestionJob.scala
@@ -146,7 +146,7 @@ class DsvIngestionJob(
             )
           } else if (compare > 0) {
             val countMissing = attributesWithoutscript.length - df.columns.length
-            throw new Exception(s"$countMissing columns in the input DataFrame ")
+            throw new Exception(s"$countMissing MISSING columns in the input DataFrame ")
           } else { // compare < 0
             val cols = df.columns
             df.select(
